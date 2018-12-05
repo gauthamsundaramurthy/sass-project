3aa4826994f18074fdd1792a9ac1cb75889de188
Gautham Sundaramurthy <gautham.s@tringapps.com>
2018-12-05 23:26:05 +0530

    Updates

---
+++ precache-manifest.72346842f20c3827e6cf7825789273e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "188bb3f9d6efd5e4f8b5",
+    "url": "/sass-project/static/js/runtime~main.188bb3f9.js"
+  },
+  {
+    "revision": "83c0437eb61be348d560",
+    "url": "/sass-project/static/js/main.83c0437e.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/sass-project/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "83c0437eb61be348d560",
+    "url": "/sass-project/static/css/main.e8ba2e6e.chunk.css"
+  },
+  {
+    "revision": "2e169d2501d07de48ffd278482ad6e86",
+    "url": "/sass-project/index.html"
+  }
+];
